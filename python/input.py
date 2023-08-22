6a6f03bfba33779a577de9fa0bee023bf7f47c2f
NotFunnyMe <128578026+NotFunnyMe@users.noreply.github.com>
2023-08-22 18:53:21 +0530

    Add files via upload

---
+++ python/input.py
@@ -0,0 +1,9 @@
+print("hola guys")
+#choose a perfect data type
+#otherwise default is string
+a=float(input("enter a number = "))
+b=float(input("enter a number = "))
+#if float only no before decimal would be added
+s=a+b
+print(s)
+
