3462215898a90b516f296a001560d22c8a00456a
NotFunnyMe <128578026+NotFunnyMe@users.noreply.github.com>
2023-08-28 14:07:49 +0530

    Add files via upload

--- python/day3.py
+++ python/day3.py
@@ -2,3 +2,17 @@ for i in range(10):
     print("count",i+5)
     if i+5>=11:
         break
+
+for i in range(10):
+    if i<5:
+        continue
+    print(i)
+
+ch=0
+while True:
+    ch=input("do u wish to continue y/n")
+    if ch=='y':
+        print("continuing")
+    if ch=='n':
+        print("exiting")
+        break
