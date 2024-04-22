812d606c43f5f528f6e945bb98035d6f705e875b
ARYAN MANDHANA <128578026+NotFunnyMe@users.noreply.github.com>
2024-04-22 19:00:09 +0530

    Add files via upload

---
+++ stringlib.py
@@ -0,0 +1,16 @@
+import string
+s = "Raj"
+s = s.lower()
+print(s)
+s = s.upper()
+print(s)
+sl = list(s)
+print(sl)
+print(s.replace("J","K"))
+print(s.replace("AJ","K"))
+l = ['a','b','c','d','e','f']
+print(l[1:4])
+print(l[:4])
+print(l[3:])
+print(l[:])
+print(l.index('d'))
